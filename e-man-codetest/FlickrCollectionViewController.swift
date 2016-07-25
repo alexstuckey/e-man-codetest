45a01f4ea6afed44424f3cf2da08ba7793d20294
Alex Stuckey <alex@stuckey.org.uk>
2016-07-25 18:07:38 +0100

    Wrote REST API logic

--- e-man-codetest/FlickrCollectionViewController.swift
+++ e-man-codetest/FlickrCollectionViewController.swift
@@ -7,50 +7,78 @@
 //
 
 import UIKit
+import Foundation
 
-private let reuseIdentifier = "PhotoCell"
-
-class FlickrCollectionViewController: UICollectionViewController {
+class FlickrCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let reuseIdentifier = "PhotoCell"
+    private let showPhotoSegueIdentifier = "ShowPicDetail"
+    
+    // Construct API URL
+    private let apiRoot = "https://api.flickr.com/services/rest/?method=flickr.photos.search"
+    private let apiConfig: [String: String] = ["api_key": "4e3145251a841b136cbcc772ba27059f",
+                                               "tags": "dog",
+                                               "per_page": "100",
+                                               "extras": "url_m",
+                                               "format": "json",
+                                               "nojsoncallback": "1"
+                                               ]
+    
+    
+    private let imageQueue = dispatch_queue_create("stuckey.e-man-codetest", nil)
+    
+    private var photos = [NSURL]()
+    private var modifiedAt = NSDate.distantPast()
+    private var cache = NSCache()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set indicator, using white to contrast with black background
+        collectionView?.infiniteScrollIndicatorStyle = .White
+        
+        // Set indicator margin, gives space for user to anticipate more content
+        collectionView?.infiniteScrollIndicatorMargin = 40
+        
         // Add infinite scroll handler
         collectionView?.addInfiniteScrollWithHandler { [weak self] (scrollView) -> Void in
-            let collectionView = scrollView as! UICollectionView
-            
-            // suppose this is an array with new data
-            let newStories = [Story]()
-            
-            var indexPaths = [NSIndexPath]()
-            let index = self?.allStories.count
-            
-            // create index paths for affected items
-            for story in newStories {
-                let indexPath = NSIndexPath(forItem: index++, inSection: 0)
-                
-                indexPaths.append(indexPath)
-                self?.allStories.append(story)
+            self?.fetchData() {
+                scrollView.finishInfiniteScroll()
             }
-            
-            // Update collection view
-            collectionView.performBatchUpdates({ () -> Void in
-                // add new items into collection
-                collectionView.insertItemsAtIndexPaths(indexPaths)
-                }, completion: { (finished) -> Void in
-                    // finish infinite scroll animations
-                    collectionView.finishInfiniteScroll()
-            });
-            
         }
+        
+        fetchData(nil)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        if identifier == showPhotoSegueIdentifier {
+            if let indexPath = collectionView?.indexPathForCell(sender as! UICollectionViewCell) {
+                let url = photos[indexPath.item]
+                if let _ = cache.objectForKey(url) {
+                    return true
+                }
+            }
+            return false
+        }
+        return true
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == showPhotoSegueIdentifier {
+            if let indexPath = collectionView?.indexPathForCell(sender as! UICollectionViewCell) {
+                let controller = segue.destinationViewController as! PicDetailViewController
+                let url = photos[indexPath.item]
+                
+                controller.photo = cache.objectForKey(url) as? UIImage
+            }
+        }
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        
+        collectionViewLayout.invalidateLayout()
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,19 +100,33 @@ class FlickrCollectionViewController: UICollectionViewController {
 
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return photos.count
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
     
         // Configure the cell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! FlickrPicCollectionViewCell
+        let url = photos[indexPath.item]
+        let image = cache.objectForKey(url) as? UIImage
+        
+        cell.flickrPicUIImageView.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        cell.flickrPicUIImageView.image = image
+        
+        if image == nil {
+            downloadPhoto(url, completion: { (url, image) -> Void in
+                let indexPath_ = collectionView.indexPathForCell(cell)
+                if indexPath.isEqual(indexPath_) {
+                    cell.flickrPicUIImageView.image = image
+                }
+            })
+        }
     
         return cell
     }
@@ -119,5 +161,102 @@ class FlickrCollectionViewController: UICollectionViewController {
     
     }
     */
+    
+    
+    private func downloadPhoto(url: NSURL, completion: (url: NSURL, image: UIImage) -> Void) {
+        dispatch_async(imageQueue, { () -> Void in
+            if let data = NSData(contentsOfURL: url) {
+                if let image = UIImage(data: data) {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.cache.setObject(image, forKey: url)
+                        completion(url: url, image: image)
+                    })
+                }
+            }
+        })
+    }
+    
+    private func fetchData(handler: (Void -> Void)?) {
+        
+        // Construct URL
+        var apiURL = apiRoot
+        for (key, value) in apiConfig {
+            apiURL = "\(apiURL)&\(key)=\(value)"
+        }
+        
+        // Form an NSURL
+        let requestURL = NSURL(string: apiURL)!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(requestURL, completionHandler: {
+            (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.handleResponse(data, response: response, error: error, completion: handler)
+                
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            });
+        })
+        
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        let delay = (photos.count == 0 ? 0 : 5) * Double(NSEC_PER_SEC)
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            task.resume()
+        })
+    }
+    
+    private func handleResponse(data: NSData!, response: NSURLResponse!, error: NSError!, completion: (Void -> Void)?) {
+        if let _ = error {
+//            showAlertWithError(error)
+            completion?()
+            return;
+        }
+        
+        var parseError: NSError?
+        var jsonString = NSString(data: data, encoding: NSUTF8StringEncoding)
+        var responseDict: [String: AnyObject]?
+        
+        // Fix broken Flickr JSON
+        jsonString = jsonString?.stringByReplacingOccurrencesOfString("\\'", withString: "'")
+        let fixedData = jsonString?.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        do {
+            responseDict = try NSJSONSerialization.JSONObjectWithData(fixedData!, options: NSJSONReadingOptions()) as? [String: AnyObject]
+        }
+        catch {
+            parseError = NSError(domain: "JSONError", code: 1, userInfo: [ NSLocalizedDescriptionKey: "Failed JSON parsing" ])
+        }
+        
+        if let parseError = parseError {
+            // Handle error properly... at some point
+            NSLog("Error: \(parseError)")
+            completion?()
+            return
+        }
+        
+        var indexPaths = [NSIndexPath]()
+        let firstIndex = photos.count
+        
+        // Pull photos out of JSON
+        if let items = responseDict?["photos"]!["photo"] as? NSArray {
+            // Find the m-type URL for the photos (best size for this demo)
+            if let urls = items.valueForKeyPath("url_m") as? [String] {
+                for (i, url) in urls.enumerate() {
+                    let indexPath = NSIndexPath(forItem: firstIndex + i, inSection: 0)
+                    
+                    photos.append(NSURL(string: url)!)
+                    indexPaths.append(indexPath)
+                }
+            }
+        }
+        
+        collectionView?.performBatchUpdates({ () -> Void in
+            self.collectionView?.insertItemsAtIndexPaths(indexPaths)
+            }, completion: { (finished) -> Void in
+                completion?()
+        });
+    }
+
 
 }
