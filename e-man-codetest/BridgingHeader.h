b0601c7c7a0551b36c6887dcbade10bc63492358
Alex Stuckey <alex@stuckey.org.uk>
2016-07-25 16:50:51 +0100

    A bridging header to cross import code from Obj-C

---
+++ e-man-codetest/BridgingHeader.h
@@ -0,0 +1,10 @@
+//
+//  BridgingHeader.h
+//  e-man-codetest
+//
+//  Created by Nigel Stuckey on 25/07/2016.
+//  Copyright © 2016 Alex Stuckey. All rights reserved.
+//
+
+
+#import <UIScrollView-InfiniteScroll/UIScrollView+InfiniteScroll.h>
