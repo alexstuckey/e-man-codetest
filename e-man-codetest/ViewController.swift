369288f170750870baf9e4a91d237aafe85605c8
Alex Stuckey <alex@stuckey.org.uk>
2016-07-25 02:22:58 +0100

    Displaying map on the location of E-Man

--- e-man-codetest/ViewController.swift
+++ e-man-codetest/ViewController.swift
@@ -7,17 +7,48 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GMSMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let camera = GMSCameraPosition.cameraWithLatitude(51.5215201,
+                                                          longitude: -0.1421055,
+                                                          zoom: 17)
+        let mapView = GMSMapView.mapWithFrame(.zero, camera: camera)
+        mapView.delegate = self
+        self.view = mapView
+        
+        let eManMarker = GMSMarker()
+        eManMarker.position = CLLocationCoordinate2DMake(51.5215201, -0.1421055)
+        eManMarker.title = "E-Man"
+        eManMarker.snippet = "Fitzrovia"
+        eManMarker.icon = UIImage(named: "map_pins.png")
+        eManMarker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+//    MARK: GMSMapViewDelegate
+    
+    func mapView(mapView: GMSMapView, willMove gesture: Bool) {
+        mapView.clear()
+    }
+    
+    func mapView(mapView: GMSMapView!, didTapMarker marker: GMSMarker!) -> Bool {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("NavViewCont") as! UIViewController
+        
+        self.presentViewController(vc, animated: true, completion: nil)
+        return false
     }
 
 
