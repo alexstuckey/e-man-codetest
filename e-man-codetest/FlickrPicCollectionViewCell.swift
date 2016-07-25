b13524bd9d49f98831cce97817199601fdbdccfd
Alex Stuckey <alex@stuckey.org.uk>
2016-07-25 12:12:16 +0100

    Created a UICollectionViewCell with an in-built UIImageView

---
+++ e-man-codetest/FlickrPicCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrPicCollectionViewCell.swift
+//  e-man-codetest
+//
+//  Created by Nigel Stuckey on 25/07/2016.
+//  Copyright © 2016 Alex Stuckey. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var flickrPicUIImageView: UIImageView!
+    
+}
